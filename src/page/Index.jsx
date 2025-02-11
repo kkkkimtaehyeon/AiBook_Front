cec057d625a8a0418df5958385659e38927ed288
kkkkimtaehyeon <kth1927@naver.com>
2025-02-11 11:58:25 +0900

    동화목록 조회, 상세 조회 페이지 구현

--- src/page/Index.jsx
+++ src/page/Index.jsx
@@ -1,11 +1,45 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function Index() {
+    const [storyList, setStoryList] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/stories")
+            .then((response) => {
+                console.log(response.data.data);
+                const responseData = response.data.data;
+                const storyList = responseData.content.map((story, index) => {
+                    return (
+                        <div key={index} onClick={() => goToStoryDetail(story.storyId)}>
+                            <span>{story.title}</span>
+                            <span>{story.memberName}</span>
+                        </div>
+                    )
+                });
+                setStoryList(storyList);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, []);
+
+    const goToStoryDetail = (storyId) => {
+        navigate(`/stories/${storyId}/pages/0`);
+    }
+
+    //TODO: 멤버 이름 누르면 멤버가 작성한 동화 목록 나오게
+
     return (
         <>
             <h1>메인 페이지입니다.</h1>
             <Link to="/login">로그인</Link>
             <Link to={"/story/new"}>동화 만들기</Link>
+            <div className={"story-container"}>
+                {storyList}
+            </div>
         </>
     );
 }
--- src/page/StoryDetail.jsx
+++ src/page/StoryDetail.jsx
@@ -1,12 +1,138 @@
-import {useParams} from "react-router-dom";
+// import {useNavigate, useParams} from "react-router-dom";
+// import {useEffect, useState} from "react";
+// import axios from "axios";
+//
+// const StoryDetail = () => {
+//     const navigate = useNavigate();
+//     const {storyId, pageNumber} = useParams();
+//     const [storyDetail, setStoryDetail] = useState({});
+//
+//     useEffect(() => {
+//         axios.get(`http://localhost:8080/api/stories/${storyId}`)
+//             .then((response) => {
+//                 if (response.data.success) {
+//                     console.log(response.data.data);
+//                     setStoryDetail(response.data.data);
+//                 }
+//             })
+//             .catch(error => console.log(error));
+//     }, [pageNumber]);
+//
+//     const renderStoryCover = () => {
+//
+//
+//     }
+//
+//     const goToNextPage = () => {
+//         const nextPageNumber = Number(pageNumber) + 1;
+//         if (nextPageNumber <= 10) {
+//             navigate(`/stories/${storyId}/pages/${nextPageNumber}`);
+//         }
+//     }
+//
+//     const goBackToPage = () => {
+//         const nextPageNumber = Number(pageNumber) - 1;
+//         if (nextPageNumber >= 0) {
+//             navigate(`/stories/${storyId}/pages/${nextPageNumber}`);
+//         }
+//     }
+//
+//     const storyCover = () => {
+//         return (
+//             <div>
+//                 <h1>{storyDetail.title}</h1>
+//                 <h3>{storyDetail.author}</h3>
+//             </div>
+//         );
+//     }
+//
+//     const storyPage = () => {
+//         const currentPage = storyDetail.pages?.find(page => page.pageNumber === Number(pageNumber)) || {};
+//
+//         return (
+//             <div>
+//                 <p>{currentPage.content || "페이지를 찾을 수 없습니다."}</p>
+//                 <p>{currentPage.pageNumber || 0}/10</p>
+//             </div>
+//         );
+//     };
+//
+//
+//
+//     return (
+//         <>
+//             {pageNumber === "0" ? storyCover() : storyPage()}
+//
+//             <button onClick={goBackToPage}>이전 페이지</button>
+//             <button onClick={goToNextPage}>다음 페이지</button>
+//         </>
+//     );
+// }
+//
+// export default StoryDetail;
+import {Link, useNavigate, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const StoryDetail = () => {
+    const navigate = useNavigate();
     const {storyId} = useParams();
-    
-    
+    const [storyDetail, setStoryDetail] = useState({});
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/api/stories/${storyId}`)
+            .then((response) => {
+                if (response.data.success) {
+                    console.log(response.data.data);
+                    setStoryDetail(response.data.data);
+                }
+            })
+            .catch(error => console.log(error));
+    }, [pageNumber]);
+
+    const goToNextPage = () => {
+        const nextPageNumber = Number(pageNumber) + 1;
+        if (nextPageNumber <= 10) {
+            setPageNumber(nextPageNumber);
+        }
+    }
+
+    const goBackToPage = () => {
+        const nextPageNumber = Number(pageNumber) - 1;
+        if (nextPageNumber >= 0) {
+            setPageNumber(nextPageNumber);
+
+        }
+    }
+
+    const storyCover = () => {
+        return (
+            <div>
+                <h1>{storyDetail.title}</h1>
+                <h3>{storyDetail.author}</h3>
+            </div>
+        );
+    }
+
+    const storyPage = () => {
+        const currentPage = storyDetail.pages?.find(page => page.pageNumber === Number(pageNumber)) || {};
+
+        return (
+            <div>
+                <p>{currentPage.content || "페이지를 찾을 수 없습니다."}</p>
+                <p>{currentPage.pageNumber || 0}/10</p>
+            </div>
+        );
+    };
+
     return (
         <>
-            <h1>동화 상세</h1>
+            {Number(pageNumber) === 0 ? storyCover() : storyPage()}
+
+            <button onClick={goBackToPage}>이전 페이지</button>
+            <button onClick={goToNextPage}>다음 페이지</button>
+            <Link to={"/"}>목록보기</Link>
         </>
     );
 }
