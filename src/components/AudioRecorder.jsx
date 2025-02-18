d543bc52e3e170b8cb1a8d6cbdf94ffe1506c397
kkkkimtaehyeon <kth1927@naver.com>
2025-02-19 07:06:41 +0900

    페이지가 바뀔 때마다 오디오 초기화 구현

--- src/components/AudioRecorder.jsx
+++ src/components/AudioRecorder.jsx
@@ -1,12 +1,23 @@
-import { useState, useRef } from "react";
-import { Button, Alert, Spinner, Container } from "react-bootstrap";
+import {useEffect, useRef, useState} from "react";
+import { Button, Container, Spinner } from "react-bootstrap";
+import useRecordStore from "../store/useRecordStore"; // Zustand 스토어 가져오기
 
-const AudioRecorder = () => {
+const AudioRecorder = ({ pageNumber }) => {
     const [isRecording, setIsRecording] = useState(false);
     const [isRecorded, setIsRecorded] = useState(false);
     const [audioURL, setAudioURL] = useState(null);
     const mediaRecorderRef = useRef(null);
     const audioChunksRef = useRef([]);
+    const { saveRecording } = useRecordStore();
+
+    // 페이지가 바뀔 때마다 오디오 초기화
+    useEffect(() => {
+        setIsRecorded(false);
+        setIsRecording(false);
+        setAudioURL(null);
+        mediaRecorderRef.current = null;
+        audioChunksRef.current = [];
+    }, [pageNumber]);
 
     const startRecording = async () => {
         try {
@@ -21,6 +32,7 @@ const AudioRecorder = () => {
                 const audioBlob = new Blob(audioChunksRef.current, { type: "audio/wav" });
                 const url = URL.createObjectURL(audioBlob);
                 setAudioURL(url);
+                saveRecording(pageNumber, audioBlob); // Zustand에 저장
                 audioChunksRef.current = [];
                 setIsRecorded(true);
             };
@@ -40,14 +52,11 @@ const AudioRecorder = () => {
 
     return (
         <Container className="text-center mt-4">
-            <h2>음성 녹음</h2>
-
-            {/* 녹음 중 표시 (Bootstrap Alert + Spinner) */}
             {isRecording && (
-                <Alert variant="danger" className="d-inline-flex align-items-center">
-                    <Spinner animation="grow" size="sm" className="me-2" />
+                <div>
+                    <Spinner variant="danger" animation="grow" size="sm" className="me-2" />
                     녹음 중...
-                </Alert>
+                </div>
             )}
 
             <div className="mt-3">
@@ -62,7 +71,7 @@ const AudioRecorder = () => {
 
             {audioURL && (
                 <div className="mt-4">
-                    <h3>녹음된 오디오</h3>
+                    <h3>미리 듣기</h3>
                     <audio controls src={audioURL} className="w-30"></audio>
                 </div>
             )}
