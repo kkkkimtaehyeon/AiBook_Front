a180113dab8755efad8ce677fd2705c9e5f472c2
kkkkimtaehyeon <kth1927@naver.com>
2025-02-20 18:29:06 +0900

    검색기능 추가

--- src/page/Index.jsx
+++ src/page/Index.jsx
@@ -1,14 +1,16 @@
 import {Link} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import axios from "axios";
-import {Card, Container, Row, Col, Button, FormControl, Dropdown} from "react-bootstrap";
+import {Button, Card, Col, Container, Dropdown, FormControl, InputGroup, Row} from "react-bootstrap";
 import Footer from "../layout/Footer.jsx";
-import {useGoToStoryDetail} from "../utils/goToStoryDetail.js";
-import {EyeFill, Heart, HeartFill} from "react-bootstrap-icons";
+import StoryList from "../components/StoryList.jsx";
 
 function Index() {
     const [storyList, setStoryList] = useState([]);
-    const goToStoryDetail = useGoToStoryDetail();
+    const searchKeyRef = useRef(null);
+    const [selectedSortOption, setSelectedSortOption] = useState(0);
+    const sortOptions = ["최신순", "오래된순", "조회수 높은순", "조회수 낮은순", "좋아요 많은 순", "좋아요 적은순"];
+
 
     useEffect(() => {
         axios.get("http://localhost:8080/api/stories")
@@ -21,52 +23,63 @@ function Index() {
             });
     }, []);
 
+    const searchStory = () => {
+        const searchKey = searchKeyRef.current.value;
+        axios.get(`http://localhost:8080/api/stories?searchTarget=title&searchKey=${searchKey}`)
+            .then(response => {
+                setStoryList(response.data.data.content);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     return (
         <>
             <Container>
-                <Card>
-                    <div>
-                        <h3>나만의 이야기를 직접 동화로 만들어 보세요!</h3>
-                        <Button as={Link} to={"/story/new"}>동화 만들기</Button>
-                    </div>
-                </Card>
+                <Row>
+                    <Card>
+                        <div>
+                            <h3>나만의 이야기를 직접 동화로 만들어 보세요!</h3>
+                            <Button as={Link} to={"/story/new"}>동화 만들기</Button>
+                        </div>
+                    </Card>
+                </Row>
 
                 {/* 검색창 및 정렬 */}
                 <Row className="mt-3 mb-3">
                     <Col xs={12} md={6}>
-                        <FormControl
-                            type="text"
-                            placeholder="제목 검색"
-                        />
+                        <InputGroup>
+                            <FormControl
+                                type="text"
+                                placeholder="제목 검색"
+                                ref={searchKeyRef}
+                            />
+                            <Button onClick={searchStory}>검색</Button>
+                        </InputGroup>
+
                     </Col>
                     <Col xs={12} md={6} className="text-md-end mt-2 mt-md-0">
                         <Dropdown>
                             <Dropdown.Toggle variant="secondary">
-                                정렬 기준
+                                {sortOptions.at(selectedSortOption)}
                             </Dropdown.Toggle>
                             <Dropdown.Menu>
-                                <Dropdown.Item>제목순</Dropdown.Item>
-                                <Dropdown.Item>작성자순</Dropdown.Item>
+                                {sortOptions.map((option, index) => (
+                                    <Dropdown.Item
+                                        key={index}
+                                        onClick={() => setSelectedSortOption(index)}
+                                    >
+                                        {option}
+                                    </Dropdown.Item>)
+                                )}
                             </Dropdown.Menu>
                         </Dropdown>
                     </Col>
                 </Row>
 
                 <Row xs={1} sm={2} md={3} lg={4} className="g-4">
-                    {storyList.map((story) => (
-                        <Col key={story.storyId}>
-                            <Card onClick={() => goToStoryDetail(story.storyId)}
-                                  className="h-100"
-                                  style={{cursor: 'pointer'}}>
-                                <Card.Body>
-                                    <Card.Title>{story.title}</Card.Title>
-                                    <Card.Text>{story.memberName}</Card.Text>
-                                    <EyeFill />{story.viewCount}
-                                    <HeartFill />{story.likeCount}
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
+                    <StoryList storyList={storyList} />
                 </Row>
             </Container>
             <Footer/>
