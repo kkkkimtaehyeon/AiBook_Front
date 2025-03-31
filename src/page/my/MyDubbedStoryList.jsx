a6b913bb24fd01b638ae1e10f894923620cbde01
kkkkimtaehyeon <kth1927@naver.com>
2025-03-31 15:55:24 +0900

    더빙한 동화목록 조회 기능 구현

--- src/page/my/MyDubbedStoryList.jsx
+++ src/page/my/MyDubbedStoryList.jsx
@@ -1,14 +1,51 @@
-import {Button} from "react-bootstrap";
+import {Button, Card, Col, Container} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import jwtAxios from "../../common/JwtAxios.js";
+import {useEffect, useState} from "react";
 
 const MyDubbedStoryList = () => {
 
+    const [dubbedStoryList, setDubbedStoryList] = useState([]);
+
+    useEffect(() => {
+        fetchMyDubbedStoryList();
+    }, []);
+
+    const fetchMyDubbedStoryList = () => {
+        jwtAxios.get("http://localhost:8080/api/stories/my/dubbed-stories")
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data.data.content);
+                    setDubbedStoryList(response.data.data.content);
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
-        <>
+        <Container>
             <Button as={Link} to={"/dubbing/new"}>
                 + 추가하기
             </Button>
-        </>
+            {dubbedStoryList.map((story) => (
+                <Col key={story.id}>
+                    <Card
+                        // onClick={() => clickHandler(story.storyId)}
+                        className="h-100"
+                        style={{cursor: 'pointer'}}
+                    >
+                        <Card.Body>
+                            <Card.Title>{story.title}</Card.Title>
+                            <Card.Text>{story.voiceName}</Card.Text>
+                            <div className="d-flex justify-content-between">
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))}
+        </Container>
     );
 }
 
